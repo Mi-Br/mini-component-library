1c709eeee86e85f35cf3678ba41fd318463305b5
Mi-Br <michailbredichin@gmail.com>
2023-10-31 20:45:07 +0100

    fixed icon pointer event, added accessability

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -19,7 +19,11 @@ const IconInput = ({
   return (
     <>
     <InputWrapper style={{'--width': width +'px'}}> 
-        <Icon  id={icon} size={STYLES[size].icon}></Icon>
+        <VisuallyHidden>
+        <Label aria-label='search'>{label}</Label>
+        </VisuallyHidden>
+        <IconWrapper><Icon  id={icon} size={STYLES[size].icon}></Icon></IconWrapper>
+        
         <Input style={{'--font-size': STYLES[size].font+'px'}} placeholder={placeholder}></Input>        
     </InputWrapper>
     </>
@@ -41,21 +45,30 @@ const STYLES = {
 
 
 const InputWrapper = styled.div`
+    position: relative;
     border-bottom: 1px solid ${COLORS.black};
     width: var(--width);
-    display: flex;
+    /* display: flex; */
     gap: 12px;
     padding: 10px;
+    padding-left: 0px;  
     &:has(Input):focus-within {
       outline: 1px solid ${COLORS.transparentGray15};
     }
   `
+
+const IconWrapper = styled.div` 
+  position: absolute;
+  left: 0;
+  margin-block: auto;
+  pointer-events: none;
+`
 const Input = styled.input`
   appearance: none;
   -webkit-appearance: none;
   -moz-appearance: none;
   border: none;
-  
+  padding-left: 38px;
   font-size: var(--font-size);
   font-weight: 700; 
   &::placeholder {
@@ -66,6 +79,8 @@ const Input = styled.input`
     outline:none;
   }
 `
+const Label = styled.label`
 
+`
 
 export default IconInput;
